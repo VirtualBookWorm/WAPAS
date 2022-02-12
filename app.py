100992759c63b7554417c376af9d013551d76f6d
Aritra Banerjee <56504168+VirtualBookWorm@users.noreply.github.com>
2022-02-12 14:33:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import streamlit as st 
-import pandas as pd 
-from PIL import Image
+import pandas as pd
+import plotly.express as px
 
 @st.cache
 def load_data(data):
 	df = pd.read_csv(data)
 	return df
+
+@st.cache
+def load_model(model_file):
+	loaded_model = joblib.load(open(os.path.join(model_file),"rb"))
+	return loaded_model			
 
 st.title("WAPAS")
 st.subheader("Web Application to Predict & Analyse Sales")
@@ -22,17 +27,29 @@ if st.button("Authenticate"):
 		data = load_data("assets/database.csv")
 		st.subheader("Exploratory Data")
 		st.dataframe(data)
-		with st.expander("See plotting..."):
-			st.write("**Scatter plots as folloing**")
-			img = Image.open('assets/images/plots.png')
-			st.image(img)
-			st.write("Therefore, **TV** adverts have the *highest* & **Newspaper** adverts have the *lowest* weight...")
+		with st.expander("See detailed plotting..."):
+			plot = px.scatter(data_frame=data, x='TV', y='Sales')
+			st.plotly_chart(plot)
+			plot = px.scatter(data_frame=data, x='Newspaper', y='Sales')
+			st.plotly_chart(plot)
+			plot = px.scatter(data_frame=data, x='Radio', y='Sales')
+			st.plotly_chart(plot)
+			st.write("***Note: Expenditure on X-axis in millions of $***")
+			st.write("***Note: Sales amount on Y-axis in Billions of $***")
+		with st.expander("See comparative analysis..."):
+			st.image('assets/images/plots.png')
+			st.write("Therefore, **TV** adverts have the *highest/most correlative* weight...")
 		st.subheader("Predict sales")
-		TV = st.slider('Spenditure on TV (million $)', 0, 300, 3)
-		Newspaper = st.slider('Spenditure on Newspaper (million $)', 0, 200, 2)
-		Radio = st.slider('Spenditure on Radio (million $)', 0, 100, 1)
-		
-		
+		#Predict
 	else:
 		st.error("Enter a valid Unique ID")
-		st.stop()
+
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+st.text("")
+
+st.markdown('<p style="color:Aqua">&copy 2022 &nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp; WAPAS v1.5.0</p>', unsafe_allow_html=True)
+st.markdown('<p style="color:Aqua">Designed &amp; developed by <a href="https://www.linkedin.com/in/aritra-ban3rjee/">Aritra Banerjee</a></p>', unsafe_allow_html=True)
